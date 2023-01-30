5ba96bc2bdd543baf46ff00fd736fed4412ebe8a
RodKal <496686@student.saxion.nl>
2023-01-30 17:38:05 +0100

    lint

--- src/components/tutorCard.tsx
+++ src/components/tutorCard.tsx
@@ -2,57 +2,58 @@ import Button from "./button";
 
 export default function TutorCard(id: number = 0) {
   // get tutor from id
-  var name: string = "Illya Brodovskyy";
-  var bio: string = "Lorem ipsum bio";
-  var subject: string = "subject";
+  const name: string = "Illya Brodovskyy";
+  const bio: string = "Lorem ipsum bio";
+  const subject: string = "subject";
 
-  var followers: number = 1000;
-  var following: number = 1000;
+  const followers: number = 1000;
+  const following: number = 1000;
 
 
   return (
     <div className="bord bg-blue-900 w-2/5 text-white">
-        {/**
+      {/**
          * 2 top section
          */}
-        <div className="p-2 w-full flex">
+      <div className="p-2 w-full flex">
 
-            {/**
+        {/**
              * image, followers and following
              */}
-            <div className="w-1/2 bord">
+        <div className="w-1/2 bord">
                 picture
-                <div className="flex items-center justify-around">
-                    <span className="p-1">Followers: {followers}</span>
-                    <span className="p-1">Follwing: {following}</span>
-                </div>
-            </div>
+          <div className="flex items-center justify-around">
+            <span className="p-1">Followers: {followers}</span>
+            <span className="p-1">Follwing: {following}</span>
+          </div>
+        </div>
 
-            {/**
+        {/**
              * description
              */}
-            <div className="w-1/2 bord">
-                {name}
-                <br/>
-                {bio}
-                <br/>
-                <Button text="View Profile" color="orange" link="/" />
-            </div>
+        <div className="w-1/2 bord">
+          {name}
+          <br/>
+          {bio}
+          <br/>
+          <Button text="View Profile" color="orange" link="/" />
+        </div>
 
-            {/**
+        {/**
              * subject and contact
              */}
-            <div className="bord w-1/2">
-                <Button text="Follow" color="blue" link="/" />
-            </div>
+        <div className="bord w-1/2">
+            {subject}
+          <Button text="Follow" color="blue" link="/" />
+        </div>
 
-            {/**
+        {/**
              * reviews
              */}
-            <div className="bord w-1/2">
+        <div className="bord w-1/2">
 
-            </div>
         </div>
+      </div>
 
     </div>
   );
