c7f9fd240660ad6a48b6a2da7f07ed30f5a9522a
Illya Brodovskyy <491391@student.saxion.nl>
2023-02-02 19:42:28 +0100

    fix: fix linting problems

--- src/pages/testSignUp.tsx
+++ src/pages/testSignUp.tsx
@@ -1,27 +1,27 @@
-import { signIn } from "next-auth/react";
-import { useState } from "react";
+import {signIn} from "next-auth/react";
+import {useState} from "react";
 
 const testSignUp = () => {
   const [firstName, setFirstName] = useState(null);
   const [lastName, setLastName] = useState(null);
   const [email, setEmail] = useState(null);
-  const [password,setPassword] = useState(null);
+  const [password, setPassword] = useState(null);
 
   const handleInputChange = (e: any) => {
-    const {id , value} = e.target;
-    if(id === "firstName"){
-        setFirstName(value);
+    const {id, value} = e.target;
+    if (id === "firstName") {
+      setFirstName(value);
     }
-    if(id === "lastName"){
-        setLastName(value);
+    if (id === "lastName") {
+      setLastName(value);
     }
-    if(id === "email"){
-        setEmail(value);
+    if (id === "email") {
+      setEmail(value);
     }
-    if(id === "password"){
-        setPassword(value);
+    if (id === "password") {
+      setPassword(value);
     }
-  }
+  };
 
   // Signs up a new user, logs him in and redirects to the testAuth page where information
   // about him is displayed
@@ -33,10 +33,10 @@ const testSignUp = () => {
         lastName: lastName,
         role: "tutor",
         email: email,
-        password: password
+        password: password,
       }),
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
     });
 
@@ -46,40 +46,40 @@ const testSignUp = () => {
       const result = await signIn("credentials", {
         email,
         password,
-        callbackUrl: "/testAuth"
+        callbackUrl: "/testAuth",
       });
 
       if (result?.error) {
         alert(result.error);
       }
     }
-  }
+  };
 
-  return(
+  return (
     <div className="form">
-        <div className="form-body">
-            <div className="username">
-                <label className="form__label">First Name </label>
-                <input onChange = {(e) => handleInputChange(e)} type="text" id="firstName" placeholder="First Name"/>
-            </div>
-            <div className="lastname">
-                <label className="form__label">Last Name </label>
-                <input  type="text" name="" id="lastName" onChange = {(e) => handleInputChange(e)}placeholder="LastName"/>
-            </div>
-            <div className="email">
-                <label className="form__label">Email </label>
-                <input  type="email" id="email" onChange = {(e) => handleInputChange(e)}placeholder="Email"/>
-            </div>
-            <div className="password">
-                <label className="form__label">Password </label>
-                <input onChange = {(e) => handleInputChange(e)} type="password"  id="password" placeholder="Password"/>
-            </div>
+      <div className="form-body">
+        <div className="username">
+          <label className="form__label">First Name </label>
+          <input onChange = {(e) => handleInputChange(e)} type="text" id="firstName" placeholder="First Name"/>
         </div>
-        <div>
-            <button onClick={async () => await handleSubmit()} type="submit">Register</button>
+        <div className="lastname">
+          <label className="form__label">Last Name </label>
+          <input type="text" name="" id="lastName" onChange = {(e) => handleInputChange(e)}placeholder="LastName"/>
         </div>
-    </div>      
-  )      
+        <div className="email">
+          <label className="form__label">Email </label>
+          <input type="email" id="email" onChange = {(e) => handleInputChange(e)}placeholder="Email"/>
+        </div>
+        <div className="password">
+          <label className="form__label">Password </label>
+          <input onChange = {(e) => handleInputChange(e)} type="password" id="password" placeholder="Password"/>
+        </div>
+      </div>
+      <div>
+        <button onClick={async () => await handleSubmit()} type="submit">Register</button>
+      </div>
+    </div>
+  );
 };
 
 export default testSignUp;
