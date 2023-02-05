1a46db11ec2eaf21839c877b2a9f7d8c2cb41db4
Illya Brodovskyy <491391@student.saxion.nl>
2023-02-05 11:09:12 +0100

    feat: add comment mongoose model

---
+++ src/models/Comment.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const CommentSchema: mongoose.Schema = new mongoose.Schema(
+    {
+      role: {type: String, required: true},
+      userId: {type: String, required: true},
+      postId: {type: String, required: true},
+      text: {type: String, required: true},
+    },
+    {timestamps: true},
+);
+
+const Comment = mongoose.models.Comment ||
+      mongoose.model("Comment", CommentSchema);
+export default Comment;
