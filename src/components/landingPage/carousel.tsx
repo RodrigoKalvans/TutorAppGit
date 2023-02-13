c803b11af927602008be1edc875f7680578cddb3
Illya Brodovskyy <491391@student.saxion.nl>
2023-02-13 16:19:47 +0100

    fix: fix eslint

--- src/components/landingPage/carousel.tsx
+++ src/components/landingPage/carousel.tsx
@@ -1,25 +1,25 @@
 import TutorCard from "./TutorCard";
 
-export default function Carousel({}) {
-    return (
-        <>
-        <div className="carousel w-full rounded-box">
-            <div id="slide1" className="carousel-item relative w-1/2 flex justify-center">
-                <TutorCard />   
-            </div> 
-            <div id="slide2" className="carousel-item relative w-1/2">
-                <TutorCard />
-            </div> 
-            <div id="slide3" className="carousel-item relative w-1/2">
-                <TutorCard />
-            </div> 
-            <div id="slide4" className="carousel-item relative w-1/2">
-                <TutorCard />
-            </div>
-            <div id="slide5" className="carousel-item relative w-1/2">
-                <TutorCard />
-            </div>
-            </div>
-        </>
-    )
+export default function Carousel() {
+  return (
+    <>
+      <div className="carousel w-full rounded-box">
+        <div id="slide1" className="carousel-item relative w-1/2 flex justify-center">
+          <TutorCard />
+        </div>
+        <div id="slide2" className="carousel-item relative w-1/2">
+          <TutorCard />
+        </div>
+        <div id="slide3" className="carousel-item relative w-1/2">
+          <TutorCard />
+        </div>
+        <div id="slide4" className="carousel-item relative w-1/2">
+          <TutorCard />
+        </div>
+        <div id="slide5" className="carousel-item relative w-1/2">
+          <TutorCard />
+        </div>
+      </div>
+    </>
+  );
 }
