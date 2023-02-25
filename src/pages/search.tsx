221c1978764d609a923d4dd22c266e459577a822
RodKal <496686@student.saxion.nl>
2023-02-25 16:57:05 +0100

    feat: search page

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,32 +1,63 @@
-import Footer from "@/components/footer";
+import Footer from "@/components/Footer";
 import Head from "next/head";
-import Navbar from "@/components/navbar";
-import { useEffect } from "react";
-import { useRouter } from "next/dist/client/router";
+import Navbar from "@/components/Navbar";
+import SearchPanel from "@/components/search/SearchPanel";
+import { useRouter } from "next/router";
 
-export default function Search({props}: {props: any}) {
+export default function Search({tutors}: {tutors:any}) {
 
+    // get search queries 
     const router = useRouter()
 
-    console.log(router.query.query)
-    console.log(router.query.role)
+    const {query: {queryFromNav, roleFromNav}}: any = router;
+
+    const navProps: {
+        query: string,
+        role: string,
+    } = {
+        query: queryFromNav,
+        role: roleFromNav,
+    }
+
+    console.log(tutors)
 
     return (
         <>
         <Head>
             <title>Search</title>
         </Head>
-        <main> 
-            <div className="bord">
-                <Navbar />
+        <main className="flex-col justify-center min-h-screen"> 
+            <div className="flex justify-center">
+                <Navbar black={true} />
             </div>
-            <div className="bord">
 
-            </div>
-            <div className="bord">
+            <SearchPanel props={navProps} users={tutors} />
+
+            <div className=" b-0">
                 <Footer />
             </div>
         </main>
         </>
     )
+}
+
+/**
+ *  this is here just for initial testing purposes, later this functionality should be changed to fetch elsewhere
+ * @returns {{tutor}[]} all tutors
+ */
+export async function getStaticProps() {
+
+    const res = await fetch("http://localhost:3000/api/tutors", {
+        method: "GET",
+        headers: {
+        "Content-Type": "application/json",
+        },
+    })
+
+    return {
+        props: {
+            tutors: await res.json()
+        }
+    }
+
 }
