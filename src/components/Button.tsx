f7b64ba68ded9559d3f00ba60b1f1bfc5a065174
Illya Brodovskyy <491391@student.saxion.nl>
2023-04-21 14:41:07 +0200

    fix: remove button element from Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,8 +9,6 @@ import React from "react";
  */
 export default function Button({children, style, link = "/"}: {children: React.ReactNode, style?: string, link?: string}) {
   return (
-    <button className={`btn btn-primary px-14 p-2 rounded-full border-0 text-white ${style}`}>
-      <Link href={link}>{children}</Link>
-    </button>
+    <Link href={link} className={`btn btn-primary px-14 p-2 rounded-full border-0 text-white ${style}`}>{children}</Link>
   );
 }
