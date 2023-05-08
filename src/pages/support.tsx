740f18de2132d040768fd0031e2cd1e57320d032
rodrigo <kalvans.rodrigo@gmail.com>
2023-05-08 21:47:33 +0200

    feat: content for about and support pages

--- src/pages/support.tsx
+++ src/pages/support.tsx
@@ -1,31 +1,31 @@
 import Navbar from "@/components/Navbar";
 import Head from "next/head";
-
-// Payments are currently handled in a no-code way
-// Simply redirect to the Stripe link for TCorvus
-// No redirecting is handled this way
+import { supportText } from "@/public/support";
+import Footer from "@/components/Footer";
 
 /**
  * Support us page
  * @return {JSX}
  */
-const SupportUs = () => {
+const Support = () => {
   return (
     <>
       <Head>
         <title>Support Us</title>
       </Head>
       <Navbar black/>
-      <div className="w-screen flex flex-col gap-20 py-20">
-        <div className="w-screen text-2xl flex justify-center">Please consider making a donation!</div>
+      <div className="w-screen flex flex-col gap-8 py-8">
         <div className="w-screen flex justify-center">
           <a href="https://donate.stripe.com/test_3csaFe1p67sL0b69AA" target="_blank" rel="noreferrer">
             <button className="btn btn-xl btn-primary">donate</button>
           </a>
         </div>
+        <p className="px-10">{supportText}</p>
+        <div className="w-screen text-2xl flex justify-center">Please consider making a donation!</div>
       </div>
+      <Footer />
     </>
   );
 };
 
-export default SupportUs;
+export default Support;
