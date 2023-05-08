ace25a6718555619b23510a06ea10367d8f86448
rodrigo <kalvans.rodrigo@gmail.com>
2023-05-08 19:59:21 +0200

    feat: price filter

--- src/components/search/Filter.tsx
+++ src/components/search/Filter.tsx
@@ -12,7 +12,9 @@ type Values = {
   role: string,
   firstName: string | undefined,
   lastName: string | undefined,
+  location: string | undefined,
   rating: number | undefined,
+  price: number | undefined,
 };
 
 const schema = Yup.object({
@@ -21,17 +23,21 @@ const schema = Yup.object({
       .max(8),
   firstName: Yup
       .string()
-      .max(25),
+      .max(25, "First name cannot be longer than 25 chars"),
   lastName: Yup
       .string()
-      .max(25),
+      .max(25, "Last name cannot be longer than 25 chars"),
   location: Yup
       .string()
-      .max(50),
+      .max(50, "Location cannot be longer than 50 chars"),
   rating: Yup
       .number()
-      .min(0)
-      .max(5),
+      .moreThan(0, "Rating cannot be <0")
+      .lessThan(5, "Rating cannot be >5")
+      .positive("Rating cannot be negative"),
+  price: Yup
+      .number()
+      .positive("Price cannot be negative"),
 });
 
 /**
@@ -52,7 +58,7 @@ export default function Filter({
 }) {
   const router: NextRouter = useRouter();
   const role = router.query.role ? router.query.role.toString() : "tutor";
-  const [chosenSubjects, setChosenSubjects] = useState<Array<any>>();
+  const [chosenSubjects, setChosenSubjects] = useState<Array<string>>();
   const [chosenLanguages, setChosenLanguages] = useState<Array<any>>();
 
   const initValues = { // for the form
@@ -61,6 +67,7 @@ export default function Filter({
     lastName: undefined,
     location: undefined,
     rating: undefined,
+    price: undefined,
   };
 
   const handleSubmit = (values: any) => {
@@ -70,7 +77,7 @@ export default function Filter({
       languages: chosenLanguages,
     };
     console.log(query);
-    setProfileState(filterProfiles(query, allUsers));
+    setProfileState(filterProfiles(query));
   };
 
   /**
@@ -89,18 +96,25 @@ export default function Filter({
  * @param {Array<any>} sourceArray is the array that will be sorted - passed by reference
  * @return {Array<any>} filtered array
  */
-  const filterProfiles = (filter: any | undefined, sourceArray: Array<any>) => {
+  const filterProfiles = (filter: any | undefined) => {
     if (filter == undefined) filter = router.query; // in case we got here from the navbar
     const keys = Object.keys(filter); // keys in the filter (eg  { firstName: "john" } )
-    const arr = sourceArray.filter((user: any) => { // do the following on every user object
+    const arr = allUsers.filter((user: any) => { // do the following on every user object
       return keys.every((key: any) => {
-        if (filter[key] == undefined || filter[key].length == 0) return true; // no value present in filter field
-        if (Array.isArray(user[key])) return filter[key].every((val: any) => user[key].includes(val)); // every value in filter must be present in user
-        if (key == "role" && filter[key] == "both") return true; // handle role == "both"
-        if (key == "rating") return filter[key] == user[key].number; // rating is a number
-        return user[key].toLowerCase().includes(filter[key].toLowerCase());
+        try {
+          if (filter[key] == undefined || filter[key] == null || filter[key].length == 0) return true; // no value present in filter field
+          if (key == "rating") return filter[key] == user[key].number; // rating is a number
+          if (key == "languages") return filter[key].every((filterLanguage: any) => user[key].some((userLanguage: any) => filterLanguage.name == userLanguage.name));
+          if (key == "price") return Object.values(user["priceForLessons"]).some((price: any) => Number(price) <= Number(filter[key])); // any price in the user object must be below entered value
+          if (key == "role" && filter[key] == "both") return true; // handle role == "both"
+          if (Array.isArray(user[key])) return filter[key].every((val: any) => user[key].includes(val)); // every value in filter must be present in user
+          return user[key].toLowerCase().includes(filter[key].toLowerCase());
+        } catch (err: any) {
+          return false;
+        }
       });
     });
+    // sort before returning
     return sortByDonation(arr);
   };
 
@@ -114,7 +128,7 @@ export default function Filter({
   };
 
   useEffect(() => {
-    setProfileState(filterProfiles(router.query, allUsers));
+    setProfileState(filterProfiles(router.query));
 
     return () => {
       setProfileState(null);
@@ -133,7 +147,7 @@ export default function Filter({
           {(formik: {
             isSubmitting: boolean | undefined;
           }) => (
-            <Form className="px-5 flex flex-col gap-2">
+            <Form className="px-5 flex flex-col gap-1">
               <h2 className="uppercase flex justify-center my-3 font-bold">filter</h2>
 
               <label
@@ -141,7 +155,7 @@ export default function Filter({
               >role
                 <Field
                   as="select"
-                  className="p-2 ml-2 rounded-xl bg-gray-300 text-black"
+                  className="p-2 px-4 ml-2 rounded-xl bg-gray-300 text-black"
                   name="role"
                 >
                   <option value="tutor" selected>Tutors</option>
@@ -150,73 +164,63 @@ export default function Filter({
                 </Field>
               </label>
 
-              <label
-                htmlFor="firstName"
-              >first name
-                <Field
-                  name="firstName"
-                  type="text"
-                  className="w-full bg-gray-300 text-gray-900"
-                />
-                <div className="text-xs text-red-600">
-                  <ErrorMessage name="firstName" />
-                </div>
-              </label>
+              <Field
+                name="firstName"
+                as="input"
+                placeholder="First name"
+                className="w-full bg-gray-300 text-gray-900"
+              />
+              <div className="text-xs text-red-600">
+                <ErrorMessage name="firstName" />
+              </div>
 
-              <label
-                htmlFor="lastName"
-              >last name
-                <Field
-                  name="lastName"
-                  type="text"
-                  className="w-full bg-gray-300 text-gray-900"
-                />
-                <div className="text-xs text-red-600">
-                  <ErrorMessage name="lastName" />
-                </div>
-              </label>
+              <Field
+                name="lastName"
+                as="input"
+                placeholder="Last name"
+                className="w-full bg-gray-300 text-gray-900"
+              />
+              <div className="text-xs text-red-600">
+                <ErrorMessage name="lastName" />
+              </div>
 
-              <label
-                htmlFor="rating"
-              >rating
-                <Field
-                  name="rating"
-                  type="numeric"
-                  className="w-full bg-gray-300 text-gray-900"
-                />
-                <div className="text-xs text-red-600">
-                  <ErrorMessage name="rating" />
-                </div>
-              </label>
+              <Field
+                name="location"
+                as="input"
+                placeholder="Location"
+                className="w-full bg-gray-300 text-gray-900"
+              />
+              <div className="text-xs text-red-600">
+                <ErrorMessage name="location" />
+              </div>
 
-              <label
-                htmlFor="location"
-              >location
-                <Field
-                  name="location"
-                  type="text"
-                  className="w-full bg-gray-300 text-gray-900"
-                />
-                <div className="text-xs text-red-600">
-                  <ErrorMessage name="location" />
-                </div>
-              </label>
+              <Field
+                name="rating"
+                as="input"
+                placeholder="Rating"
+                className="w-full bg-gray-300 text-gray-900"
+              />
+              <div className="text-xs text-red-600">
+                <ErrorMessage name="rating" />
+              </div>
 
-              <label
-                htmlFor="subjects"
-              >Subjects
-                <SubjectSelect setFunction={setChosenSubjects} subjects={subjects} />
-              </label>
-              <div className="text-red-600 text-sm">
+              <Field
+                name="price"
+                as="input"
+                placeholder="Maximum price"
+                className="w-full bg-gray-300 text-gray-900"
+              />
+              <div className="text-xs text-red-600">
+                <ErrorMessage name="price" />
+              </div>
+
+              <SubjectSelect setSubjectsState={setChosenSubjects} subjects={subjects} />
+              <div className="text-sm text-red-600">
                 <ErrorMessage name="subjects" />
               </div>
 
-              <label
-                htmlFor="languages"
-              >Languages
-                <LanguageSelect setFunction={setChosenLanguages} />
-              </label>
-              <div className="text-red-600 text-sm">
+              <LanguageSelect setLanguagesState={setChosenLanguages} />
+              <div className="text-sm text-red-600">
                 <ErrorMessage name="languages" />
               </div>
 
