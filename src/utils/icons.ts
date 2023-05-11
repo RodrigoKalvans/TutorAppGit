adab9457e9e44073efce75f2100f9c5bd4a8ac23
rodrigo <kalvans.rodrigo@gmail.com>
2023-05-11 10:30:30 +0200

    feat: subject icons

--- src/utils/icons.ts
+++ src/utils/icons.ts
@@ -1,8 +1,32 @@
 import {TbMathSymbols} from "react-icons/tb";
-import {MdHistoryEdu, MdEmail, MdDelete} from "react-icons/md";
-import {AiFillPhone, AiOutlineEdit, AiFillLike, AiOutlineLike, AiOutlineLoading, AiFillStar} from "react-icons/ai";
-import {IoSchoolOutline} from "react-icons/io5";
+import {MdHistoryEdu, MdEmail, MdDelete, MdBiotech} from "react-icons/md";
+import {AiFillPhone, AiOutlineQuestion, AiOutlineEdit, AiFillLike, AiOutlineLoading, AiFillStar} from "react-icons/ai";
+import {IoSchoolOutline, IoEarthOutline, IoLanguageOutline} from "react-icons/io5";
 import {FaArrowCircleUp} from "react-icons/fa";
 import {CgProfile} from "react-icons/cg";
+import {BiAtom, BiCodeAlt, BiBook} from "react-icons/bi";
+import {SlChemistry} from "react-icons/sl";
 
-export {CgProfile as ProfileIcon, AiFillStar as StarIcon, FaArrowCircleUp as PromoIcon, TbMathSymbols as MathIcon, MdHistoryEdu as HistoryIcon, AiFillPhone as PhoneIcon, MdEmail as EmailIcon, IoSchoolOutline as SchoolIcon, AiOutlineEdit as EditIcon, AiFillLike as LikeIcon, AiOutlineLike as LikeIconPlaceholder, AiOutlineLoading as LoadingIcon, MdDelete as DeleteIcon};
+export {
+  TbMathSymbols as MathIcon,
+  MdHistoryEdu as HistoryIcon,
+  BiAtom as PhysicsIcon,
+  BiBook as LiteratureIcon,
+  SlChemistry as ChemistryIcon,
+  IoEarthOutline as GeographyIcon,
+  BiCodeAlt as ProgrammingIcon,
+  IoLanguageOutline as EnglishIcon,
+  MdBiotech as BiologyIcon,
+
+  AiOutlineQuestion as QuestionIcon,
+  CgProfile as ProfileIcon,
+  AiFillStar as StarIcon,
+  FaArrowCircleUp as PromoIcon,
+  AiFillPhone as PhoneIcon,
+  MdEmail as EmailIcon,
+  IoSchoolOutline as SchoolIcon,
+  AiOutlineEdit as EditIcon,
+  AiFillLike as LikeIcon,
+  AiOutlineLoading as LoadingIcon,
+  MdDelete as DeleteIcon,
+};
