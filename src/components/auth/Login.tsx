a5cb32df6d2ea3d40b65c334829920c083338a05
rodrigo <kalvans.rodrigo@gmail.com>
2023-05-12 15:02:51 +0200

    change: price UI on signup

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -10,9 +10,22 @@ interface Values {
   password: string,
 }
 
+const schema = Yup.object({
+  email: Yup.string()
+      .max(30, "Must be 30 characters or less")
+      .email("Invalid email address")
+      .required("Please enter your email"),
+  password: Yup.string()
+      .required("Please enter your password"),
+});
+
+const initValues = {
+  email: "",
+  password: "",
+};
+
 /**
- * TODO: fill this in
- * @param {any} param0
+ * This component is displayed to the user on the signin page during login, not sign-up
  * @return {any}
  */
 export default function Login() {
@@ -20,88 +33,65 @@ export default function Login() {
   const loading = useRef<boolean>(false);
   const router = useRouter();
 
+  const handleSubmit = async (values: any) => {
+    loading.current = true;
+    const res = await signIn("credentials", {
+      email: values.email,
+      password: values.password,
+      redirect: false,
+    });
+
+    if (res?.error) {
+      setError(res.error);
+      loading.current = false;
+    } else {
+      const session: Session | null = await getSession();
+
+      router.push(`/${session?.user.role}s/${session?.user.id}`);
+    }
+  };
+
   return (
     <>
       {/** abstract onSubmit to a function */}
       <Formik
-        initialValues={{email: "", password: ""}}
-        validationSchema={Yup.object({
-          email: Yup.string()
-              .max(30, "Must be 30 characters or less")
-              .email("Invalid email address")
-              .required("Please enter your email"),
-          password: Yup.string().required("Please enter your password"),
-        })}
-        onSubmit={async (values: Values) => {
-          loading.current = true;
-          const res = await signIn("credentials", {
-            email: values.email,
-            password: values.password,
-            redirect: false,
-          });
-
-          if (res?.error) {
-            setError(res.error);
-            loading.current = false;
-          } else {
-            const session: Session | null = await getSession();
-
-            router.push(`/${session?.user.role}s/${session?.user.id}`);
-          }
-        }}
+        initialValues={initValues}
+        validationSchema={schema}
+        onSubmit={async (values: Values) => handleSubmit(values)}
       >
-        {() => (
+        {(formik: {
+            isSubmitting: boolean | undefined;
+        }) => (
           <Form>
-            <div >
+            <div className="flex flex-col ">
               <div className="text-red-400 text-md text-center rounded p-2">
                 {error}
               </div>
-              <div className="mb-4">
-                <label
-                  htmlFor="email"
-                  className="uppercase text-sm text-gray-600 font-bold"
-                >
-                    Email
-                  <Field
-                    name="email"
-                    aria-label="enter your email"
-                    aria-required="true"
-                    type="text"
-                    className="w-full bg-gray-300 text-gray-900 mt-2 p-3"
-                  />
-                </label>
-                <div className="text-red-600 text-sm">
-                  <ErrorMessage name="email" />
-                </div>
+              <Field
+                name="email"
+                as="input"
+                placeholder="Email"
+                className="w-full bg-gray-300 text-gray-900 mt-2 p-3"
+              />
+              <div className="text-red-600 text-sm">
+                <ErrorMessage name="email" />
               </div>
-              <div className="mb-6">
-                <label
-                  htmlFor="password"
-                  className="uppercase text-sm text-gray-600 font-bold"
-                >
-                    password
-                  <Field
-                    name="password"
-                    aria-label="enter your password"
-                    aria-required="true"
-                    type="password"
-                    className="w-full bg-gray-300 text-gray-900 mt-2 p-3"
-                  />
-                </label>
-                <div className="text-red-600 text-sm">
-                  <ErrorMessage name="password" />
-                </div>
+              <Field
+                name="password"
+                type="password"
+                as="input"
+                placeholder="Password"
+                className="w-full bg-gray-300 text-gray-900 mt-2 p-3"
+              />
+              <div className="text-red-600 text-sm">
+                <ErrorMessage name="password" />
               </div>
-              <div className="flex items-center justify-center">
-                <button
-                  type="submit"
-                  disabled={loading.current}
-                  className="bg-orange-500 text-gray-100 p-3 rounded-lg w-full"
-                >
-                  {loading.current ? "Logging in..." : "Sign In"}
-                </button>
-              </div>
-
+              <button
+                type="submit"
+                className="btn bg-orange-500 hover:bg-orange-600 my-2 rounded-lg w-full"
+              >
+                {formik.isSubmitting ? "Logging in..." : "Sign In"}
+              </button>
             </div>
           </Form>
         )}
