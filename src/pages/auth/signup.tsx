2322c330b4cb3e5d9a05ee846ed32777ae358a16
Illya Brodovskyy <491391@student.saxion.nl>
2023-05-16 16:33:13 +0200

    feat: restyle and fix sign up

--- src/pages/auth/signup.tsx
+++ src/pages/auth/signup.tsx
@@ -9,26 +9,42 @@ const SignUpPage = ({subjects}: {subjects: any[]}) => {
   return (
     <>
       <Head>
-        <title>Sign in</title>
+        <title>Sign up</title>
       </Head>
 
-      <main className="container">
-        <div className="flex flex-col items-center justify-center min-h-screen bg-white h-full">
-          <Image
-            src="/logo.png"
-            width={100}
-            height={100}
-            alt={"logo"}
-          />
+      <main className="flex min-h-screen h-full bg-blue-900">
+        <div className="flex flex-col items-center justify-center w-full bg-white">
+          <Link href="/">
+            <Image
+              src="/logo.png"
+              width={70}
+              height={70}
+              alt={"logo"}
+              style={{
+                position: "fixed",
+                top: 0,
+                left: 0,
+                margin: "0.5rem",
+              }}
+            />
+          </Link>
 
-          <div className="w-1/4 mt-7 text-center">
-            <h1 className="text-3xl mb-5">Sign up</h1>
+          <div className="w-3/5 mt-7 text-center">
+            <h1 className="text-2xl mb-4 md:hidden">Sign up</h1>
             <Signup subjects={subjects} />
             <p className="text-sm">
-              Already have an account? <Link href="/auth/signin" className="underline">Sign in</Link>
+              Already have an account? <Link href="/auth/signin" className="underline hover:opacity-80 transition">Sign in</Link>
             </p>
           </div>
 
+        </div>
+
+        <div className="min-h-screen h-full w-full  hidden md:flex items-center">
+
+          <div className="ml-8">
+            <p className="font-black text-6xl text-light">Create an account.</p>
+            <p className="font-medium text-light">Thank you for visiting, dear guest! Complete the sign up process and use our platform to the fullest.</p>
+          </div>
         </div>
       </main>
     </>
