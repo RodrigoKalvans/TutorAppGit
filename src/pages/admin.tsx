d17a128093917ad7cc976bb4572954f68174a0f6
rodrigo <kalvans.rodrigo@gmail.com>
2023-06-04 18:58:10 +0200

    fix: capitalization bug

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -112,7 +112,7 @@ const Admin = (
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        name,
+        name: name.charAt(0).toUpperCase() + name.slice(1), // capitalize first letter
       }),
     });
     console.log(await response.json());
